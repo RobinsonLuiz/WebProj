d0c3798272d1c23728710802b38ed7c39244faa1
Robinson Luiz <robinsontads@outlook.com>
2019-03-10 19:33:19 -0300

    fix private variables

--- src/App.ts
+++ src/App.ts
@@ -10,32 +10,40 @@ import Helpers from "../src/helpers/Helpers";
 
 class App {
   
-  public express: any;
+  private _express: any;
   private _session: any;
   private _store: any;
   private _cookie: any;
 
   constructor() {
-    this.express = express();
+    this._express = express();
     this.middlewares();
     this.routes();
+  }
+
+
+  /**
+   * Express load
+   */
+  getExpress() {
+    return this._express;
   }
 
   /**
    * Middlewares para funcionamento do express
    */
   middlewares() {
-    this.express.set("view engine", "ejs");
-    this.express.use(bodyParser.json());
-    this.express.use(bodyParser.urlencoded({ extended: true }));
-    this.express.use(express.static(__dirname + "/public"));
-    this.express.set("views", __dirname + "\\public\\views");
+    this._express.set("view engine", "ejs");
+    this._express.use(bodyParser.json());
+    this._express.use(bodyParser.urlencoded({ extended: true }));
+    this._express.use(express.static(__dirname + "/public"));
+    this._express.set("views", __dirname + "\\public\\views");
     this._session = Helpers.hashCode("session-storage-rbn") + "-session-rbn";
     this._store = new session.MemoryStore();
     this._cookie = cookieParser(this._session);
-    this.express.use(this._cookie);
-    this.express.set('trust proxy', 1);
-    this.express.use(
+    this._express.use(this._cookie);
+    this._express.set('trust proxy', 1);
+    this._express.use(
       session({
         name: "session-storage",
         secret: String(this._session),
@@ -50,9 +58,9 @@ class App {
    * Rotas da aplicação
    */
   routes() {
-    new IndexRoute(this.express);
-    new SceneRoute(this.express);
-    new GameRoute(this.express);
+    new IndexRoute(this._express);
+    new SceneRoute(this._express);
+    new GameRoute(this._express);
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { Env_Variables } from './config/Env_Variables';
 import App from './App';
 
 //inicio da aplicação
-App.express.listen(Env_Variables.PORT, () => console.log(`server rodando na porta ${Env_Variables.PORT}`));
+App.getExpress().listen(Env_Variables.PORT, () => console.log(`server rodando na porta ${Env_Variables.PORT}`));
