ba3a7fed14f02e4cd488a5df5d1565f6c23531eb
Robinson Luiz <robinsontads@outlook.com>
2019-03-13 08:12:50 -0300

    Update drag multiple types

--- src/public/game/boxColider.js
+++ src/public/game/boxColider.js
@@ -1,28 +1,35 @@
 function boxColider(box, boxClone, stage, layer) {
-    boxClone.on("dragstart", function () {
-        let xtest = boxClone.x();
-        let ytest = boxClone.y();
-        boxClone.on("dragend", function (evt) {
-            if (!document.querySelector("#live")) {
-                if (
-                    (box.x() - boxClone.x()) <= 25
-                    && ((box.x() - boxClone.x()) > 0)
-                    && ((box.y() - boxClone.y()) <= 5)
-                    && ((box.y() - boxClone.y()) > 0)) {
-                    box.fill(boxClone.fill());
-                    boxClone.destroy();
-                    try {
-                        tr.destroy();
-                    } catch (err) {
+  boxClone.on("dragstart", function() {
+    let xtest = boxClone.x();
+    let ytest = boxClone.y();
+    boxClone.on("dragend", function(evt) {
+      if (!document.querySelector("#live")) {
+        let s1 = box.getClientRect(); // use this to get bounding rect for shapes other than rectangles.
+        let s2 = boxClone.getClientRect();
 
-                    }
-                    layer.draw();
-                } else {
-                    boxClone.x(xtest);
-                    boxClone.y(ytest);
-                    layer.draw();
-                }
-            }
-        });
+        let X = s1.x;
+        let Y = s1.y;
+        let A = s1.x + s1.width;
+        let B = s1.y + s1.height;
+
+        let X1 = s2.x;
+        let A1 = s2.x + s2.width;
+        let Y1 = s2.y;
+        let B1 = s2.y + s2.height;
+
+        if (A < X1 || A1 < X || B < Y1 || B1 < Y) {
+          box.fill(boxClone.fill());
+          boxClone.destroy();
+          try {
+            tr.destroy();
+          } catch (err) {}
+          layer.draw();
+        } else {
+          boxClone.x(xtest);
+          boxClone.y(ytest);
+          layer.draw();
+        }
+      }
     });
+  });
 }
--- src/public/game/load.js
+++ src/public/game/load.js
@@ -16,6 +16,7 @@ async function init(param) {
     );
     stage.id = param;
     let fieldsJson = JSON.parse(json[0].scene ? json[0].scene : json[0]);
+    layer = stage.children[0];
     container = stage.container();
     container.focus();
     container.addEventListener("keydown", function(e) {
@@ -32,8 +33,74 @@ async function init(param) {
         } catch (err) {}
       }
     });
+    var mode;
+    // draw a rectangle to be used as the rubber area
+    var r2 = new Konva.Rect({
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      stroke: "red",
+      dash: [2, 2]
+    });
+    r2.listening(false); // stop r2 catching our mouse events.
+    layer.add(r2);
+    // start the rubber drawing on mouse down.
+    container.addEventListener("mousedown", function(e) {
+      mode = "drawing";
+      startDrag({ x: e.screenX, y: e.screenY });
+    });
+
+    // update the rubber rect on mouse move - note use of 'mode' var to avoid drawing after mouse released.
+    container.addEventListener("mousemove", function(e) {
+      if (mode === "drawing") {
+        updateDrag({ x: e.screenX, y: e.screenY});
+      }
+    });
+
+    container.addEventListener("mouseup", function(e) {
+      mode = "";
+      r2.visible(false);
+      layer.draw();
+    });
+
+    function startDrag(posIn) {
+      posStart = { x: posIn.x, y: posIn.y };
+      posNow = { x: posIn.x, y: posIn.y };
+    }
+
+    function updateDrag(posIn) {
+      // update rubber rect position
+      posNow = { x: posIn.x, y: posIn.y };
+      console.log(posStart, posNow);
+      var posRect = reverse(posStart, posNow);
+      r2.x(posRect.x1 - 180);
+      r2.y(posRect.y1 - 300);
+      r2.width(posRect.x2 - posRect.x1);
+      r2.height(posRect.y2 - posRect.y1);
+      r2.visible(true);
+      layer.draw();
+    }
+
+    function reverse(r1, r2) {
+      var r1x = r1.x,
+        r1y = r1.y,
+        r2x = r2.x,
+        r2y = r2.y,
+        d;
+      if (r1x > r2x) {
+        d = Math.abs(r1x - r2x);
+        r1x = r2x;
+        r2x = r1x + d;
+      }
+      if (r1y > r2y) {
+        d = Math.abs(r1y - r2y);
+        r1y = r2y;
+        r2y = r1y + d;
+      }
+      return { x1: r1x, y1: r1y, x2: r2x, y2: r2y }; // return the corrected rect.
+    }
     let stageFor = stage.children[0].children;
-    layer = stage.children[0];
     for (let i = 0; i < stageFor.length; i += 2) {
       let box;
       let boxClone;
@@ -108,6 +175,7 @@ function createText(letter, size, fontFamily, x, y) {
   tboxClone.on("mouseout", function() {
     document.body.style.cursor = "default";
   });
+
   boxColider(tbox, tboxClone, stage, layer);
   transformBox(tbox, tboxClone, stage, layer);
   layer.add(tbox);
